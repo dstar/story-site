ec2a471def356611d93a3c8d6d8c004c63f92eb9
calvertvl <calvertvl@7189bf43-9712-0410-8374-e18a92618762>
2006-05-21 02:05:08 +0000

    Removed duplicate header on playground main page, and fixed 'Prev' link for chapter view.
    git-svn-id: svn://pele.cx/site/story/trunk@21 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -4,7 +4,7 @@ module ChaptersHelper
     Chapter.find(:first, :conditions => ["story_id = ? and number = ?",chapter.story_id, number])
   end
   def prevChapter(chapter)
-    number = Chapter.count_by_sql(["select min(number) from chapters where story_id = ? and number < ?",chapter.story_id, chapter.number])
+    number = Chapter.count_by_sql(["select max(number) from chapters where story_id = ? and number < ?",chapter.story_id, chapter.number])
     Chapter.find(:first, :conditions => ["story_id = ? and number = ?",chapter.story_id, number])
   end
 end
