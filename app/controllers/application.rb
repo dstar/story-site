76993d39ed340e8a3462139dbd8be570685477b9
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2006-09-30 20:35:59 +0000

    .
    git-svn-id: svn://pele.cx/site/story/trunk@64 7189bf43-9712-0410-8374-e18a92618762

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -12,9 +12,9 @@ class ApplicationController < ActionController::Base
 
   before_filter :setup_auth_structures 
   before_filter :setup_authorize_hash 
-  logger.debug "QQQ: authenticating before destroying #{@story.id}\n" if params[:action] == "destroy"
+  debug_destroy "QQQ: authenticating before destroying #{@story.id}\n"
   before_filter :authenticate
-  logger.debug "QQQ: finished authenticating before destroying #{@story.id}\n" if params[:action] == "destroy"
+  debug_destroy "QQQ: finished authenticating before destroying #{@story.id}\n"
 
   before_filter do |c|
     c.story = Story.find_by_short_title(c.request.subdomains(0).first) unless c.story
@@ -113,4 +113,7 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def debug_destroy(message)
+    logger.debug "#{message}\n" if params[:action] == "destroy"
+  end
 end
