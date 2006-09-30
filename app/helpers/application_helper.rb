d4d213f28079c5c12e82fd3f6096bbf74c5f0e8b
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2006-09-30 23:40:42 +0000

    Fixed StoryHost to work with arbitrary length subdomains.
    git-svn-id: svn://pele.cx/site/story/trunk@87 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   def StoryHost(story_id)
+    domain_length = request.subdomains.length
     unless story_id == "playground"
-      Story.find(story_id).short_title.concat('.').concat(request.domain).concat(request.port_string)
+      Story.find(story_id).short_title.concat('.').concat(request.domain(domain_length)).concat(request.port_string)
     else
-      story_id.concat('.').concat(request.domain).concat(request.port_string)
+      story_id.concat('.').concat(request.domain(domain_length)).concat(request.port_string)
     end
   end
 end
