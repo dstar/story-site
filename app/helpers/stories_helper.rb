2dc61e228d515dd15c8042011bc6b7357edbf43f
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2006-10-01 06:42:37 +0000

    Don't include draft words in wordcount except for betas and authors
    git-svn-id: svn://pele.cx/site/story/trunk@138 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,5 +1,9 @@
 module StoriesHelper
   def Wordcount(story_id)
-    Story.count_by_sql ["select sum(words) from chapters where story_id = ?", story_id]
+    if @authinfo[:username] and ( StoryPermission.has_permission(User.find(@authinfo[:user_id]), { 'id' => story_id, 'permission' => 'author'}) or StoryPermission.has_permission(User.find(@authinfo[:user_id]), { 'id' => story_id, 'permission' => 'beta-reader'}) )
+      Story.count_by_sql ["select sum(words) from chapters where story_id = ?", story_id]
+    else
+      Story.count_by_sql ["select sum(words) from chapters where story_id = ? AND status = 'released'", story_id]
+    end
   end
 end
