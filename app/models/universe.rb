c017a6b8de8722777f7f1e5681999b00f5f694df
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2006-10-01 05:27:43 +0000

    _Now_ I'm sorting by story count.
    git-svn-id: svn://pele.cx/site/story/trunk@124 7189bf43-9712-0410-8374-e18a92618762

--- app/models/universe.rb
+++ app/models/universe.rb
@@ -3,4 +3,11 @@ class Universe < ActiveRecord::Base
 
   has_many :universe_permissions
 
+  def self.OrderedListByStoryCount
+    find(:all,
+            :select => "name, description, count(stories.id) as sort, universes.id",
+            :joins => "left outer join stories on story.universe_id = universes.id",
+            :group => "universes.id", :order => "sort asc")
+  end
+
 end
