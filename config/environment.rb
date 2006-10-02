35c97179612fd0e7a45ad14f5e40fc06895a0d0b
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2006-10-02 18:44:09 +0000

    Switch to memcached
    git-svn-id: svn://pele.cx/site/story/trunk@243 7189bf43-9712-0410-8374-e18a92618762

--- config/environment.rb
+++ config/environment.rb
@@ -10,6 +10,17 @@ RAILS_GEM_VERSION = '1.1.2'
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
+require 'memcache'
+
+memcache_options = {
+   :compression => false,
+   :debug => false,
+   :namespace => "app-#{RAILS_ENV}",
+   :readonly => false,
+   :urlencode => false
+}
+memcache_servers = [ '127.0.0.1:11211',]
+
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence those specified here
 
@@ -25,7 +36,7 @@ Rails::Initializer.run do |config|
 
   # Use the database for sessions instead of the file system
   # (create the session table with 'rake db:sessions:create')
-  config.action_controller.session_store = :active_record_store
+  config.action_controller.session_store = :mem_cache_store
   config.action_controller.session :domain => 'pele.cx'
   # Use SQL instead of Active Record's schema dumper when creating the test database.
   # This is necessary if your schema can't be completely dumped by the schema dumper,
