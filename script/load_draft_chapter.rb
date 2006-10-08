6b0e65dfbf409d71775a4cad4f6d5c67429c0ea2
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2006-10-08 16:59:22 +0000

    Commit theme edit files; commit para delete conf page; set ignores on various dirs
    git-svn-id: svn://pele.cx/site/story/trunk@337 7189bf43-9712-0410-8374-e18a92618762

---
+++ script/load_draft_chapter.rb
@@ -0,0 +1,74 @@
+class LoadChapter
+  def LoadChapter.process_file(file, chapter_id, filename)
+    @chapter = Chapter.find(chapter_id)
+
+    @chapter.paragraphs.each {|para| para.pcomments.each { |p| p.destroy } } #kill any pre-existing comments
+    @chapter.paragraphs.each {|p| p.destroy} #kill any pre-existing paras
+
+    paragraph_buffer = String.new
+    para_count = 1
+    word_count = 0
+    file_string = file.read
+    file_string.gsub!(/\r/,'')
+    lines = file_string.split(/\n\n/)
+    lines.each { |line|
+      line.gsub!(/^\n/,' ')
+      line.gsub!(/(\w)\n(\w)/,'\1 \2')
+      line.gsub!(/\n/,'')
+      line.gsub!(/^\s*|\s*$/,'')
+      line.gsub!(/#/,'***') if line == "#"
+      line.gsub!(/\s+--/, "--")
+      word_count += line.split.length
+      
+      para = Paragraph.new()
+      para.body = line
+      para.position = para_count
+      para.chapter_id = chapter_id
+      
+      para.save
+      
+      para_count += 1
+    }
+    
+    @chapter.words = word_count
+    html_filename1 = filename
+    html_filename2 = html_filename1.gsub(/.*\//, "")
+    html_filename3 = html_filename2.gsub(/\..*/, ".html")
+    @chapter.file = html_filename3
+    @chapter.save
+  end
+  
+end
+
+
+RAILS_ENV = 'production'
+require File.dirname(__FILE__) + '/../config/environment'
+
+title = ARGV[0]
+chapter_num = ARGV[1]
+date = ARGV[2]
+filename = ARGV[3]
+
+@story = Story.find_by_short_title(title)
+unless @story
+  puts "Could not find story!"
+  Kernel::exit 1
+end
+
+@chapter = Chapter.find_by_story_id_and_number(@story.id, chapter_num)
+@chapter = Chapter.new() unless @chapter
+
+@chapter.date = date
+@chapter.story_id = @story.id
+@chapter.number = chapter_num
+@chapter.status = 'draft'
+
+file = File.new(filename)
+
+if @chapter.save
+  LoadChapter.process_file(file,@chapter.id,filename)
+else
+  puts "Could not save chapter!"
+  Kernel::exit 1
+end
+
