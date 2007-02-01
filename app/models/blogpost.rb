54b8b31cdf2d40f9e40a7a8b10447f4936317987
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-02-01 21:33:10 +0000

    Fix blogpost order
    git-svn-id: svn://pele.cx/site/story/trunk@366 7189bf43-9712-0410-8374-e18a92618762

--- app/models/blogpost.rb
+++ app/models/blogpost.rb
@@ -4,7 +4,7 @@ class Blogpost < ActiveRecord::Base
 
   def self.frontpagelist
     find(:all,
-      :order => 'created_on desc', 
+      :order => 'created_on desc, id asc', 
       :limit => '10')
   end
 end
