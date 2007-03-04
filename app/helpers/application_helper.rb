d6467d0010586c6af70c486c451c8d9867205b62
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-03-04 02:01:35 +0000

    remove random missed debugging code
    git-svn-id: svn://pele.cx/site/story/trunk@396 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,7 +11,7 @@ module ApplicationHelper
 
   def our_markdown(text)
     text.gsub!(/\s*--\s*/,"&mdash; ")
-    logger.info("Marking down! #{text}\n")
+#    logger.info("Marking down! #{text}\n")
     markdown(text)
   end
 
