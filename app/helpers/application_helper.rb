fe83009c40749ba04c02c31a518e76957e3a5562
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-08-07 23:12:08 +0000

    Fixing minor issues with styles, fixing thinko in disabling markdown() in our_markdown()
    git-svn-id: svn://pele.cx/site/story/trunk@470 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
   end
 
   def our_markdown(text)
-    text.gsub!(/\s*--\s*/,"&mdash; ")
+    text.gsub(/\s*--\s*/,"&mdash; ")
 #    logger.info("Marking down! #{text}\n")
 #    markdown(text)
   end
