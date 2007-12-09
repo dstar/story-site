faae4f52e43aa604f2b3316eb55446d9eb6dd82e
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-09 18:52:33 +0000

    clean up views/chapters
    git-svn-id: svn://pele.cx/site/story/trunk@631 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -6,4 +6,47 @@ module StoriesHelper
       Story.count_by_sql ["select sum(words) from chapters where story_id = ? AND status = 'released'", story_id]
     end
   end
+
+  def nav_links(story)
+    nav_buffer = ""
+    if @authinfo[:user] and (story and @authinfo[:user].has_universe_permission(@universe.id,'owner') or @authinfo[:user].has_story_permission(story.id,'author') )
+      nav_buffer += link_to 'Edit Story Permissions', :controller => 'stories', :action => 'permissions', :id => story.id
+      nav_buffer +=  " | "
+      nav_buffer += link_to 'Edit Story', :action => 'edit', :id => story
+      nav_buffer += link_to 'Delete Story', :action => 'delete_story', :id => story
+      nav_buffer +=  " | "
+      link_to 'Add Chapter', :controller => 'chapters', :action => 'new', :story_id => story.id
+    end
+    return nav_buffer
+  end
+
+  def can_delete?(story)
+    if @authinfo[:user] and (story and @authinfo[:user].has_universe_permission(@universe.id,'owner') or @authinfo[:user].has_story_permission(story.id,'author') )
+      return true
+    else
+      return false
+    end
+  end
+
+  def can_comment(chapter)
+    if is_author(chapter) or is_beta_reader(chapter)
+      return true
+    end
+    return false
+  end
+
+  def is_author(chapter)
+    if @authinfo[:user] and @authinfo[:user_id].has_story_permission(chapter.story.id,'author')
+      return true
+    end
+    return false
+  end
+
+  def is_beta_reader(chapter)
+    if @authinfo[:user] and @authinfo[:user_id].has_story_permission(chapter.story.id,'beta-reader')
+      return true
+    end
+    return false
+  end
+
 end
