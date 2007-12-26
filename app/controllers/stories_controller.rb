b5aba214541274e2a4d12447132a223e733a0259
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-26 17:43:54 +0000

    Debug Playground auth handling
    git-svn-id: svn://pele.cx/site/story/trunk@672 7189bf43-9712-0410-8374-e18a92618762

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -23,7 +23,7 @@ class StoriesController < ApplicationController
   end
 
   def check_authorization(user)
-    if request.subdomains(0).first == 'playground' 
+    if request.subdomains(0).first == 'playground' and params[:action] == 'show'
       return true
     end
     logger.debug "SubD is #{request.subdomains(0).first}, action is #{params[:action]}"
