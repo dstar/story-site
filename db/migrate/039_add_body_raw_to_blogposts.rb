6991bbede0130e2aaa84c1eee0820b5c8a443402
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-27 23:17:45 +0000

    fix blogpost and pcomment migration
    git-svn-id: svn://pele.cx/site/story/trunk@768 7189bf43-9712-0410-8374-e18a92618762

--- db/migrate/039_add_body_raw_to_blogposts.rb
+++ db/migrate/039_add_body_raw_to_blogposts.rb
@@ -1,14 +1,14 @@
 class AddBodyRawToBlogposts < ActiveRecord::Migration
   def self.up
     add_column :blogposts, :body_raw, :text
-    Blogposts.find(:all).each do |b|
+    Blogpost.find(:all).each do |b|
       b.body_raw = b.body.dup
       b.save
     end
   end
 
   def self.down
-    Blogposts.find(:all).each do |b|
+    Blogpost.find(:all).each do |b|
       b.body = b.body_raw.dup
       b.save
     end
