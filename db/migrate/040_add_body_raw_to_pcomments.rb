a51cb93c38fb56678ba952286b72c0c79030854c
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-27 23:17:06 +0000

    fix blogpost and pcomment migration
    git-svn-id: svn://pele.cx/site/story/trunk@767 7189bf43-9712-0410-8374-e18a92618762

--- db/migrate/040_add_body_raw_to_pcomments.rb
+++ db/migrate/040_add_body_raw_to_pcomments.rb
@@ -1,14 +1,14 @@
 class AddBodyRawToPcomments < ActiveRecord::Migration
   def self.up
     add_column :pcomments, :body_raw, :text
-    Pcomments.find(:all).each do |pc|
+    Pcomment.find(:all).each do |pc|
       pc.body_raw = pc.body.dup
       pc.save
     end
   end
 
   def self.down
-    Pcomments.find(:all).each do |pc|
+    Pcomment.find(:all).each do |pc|
       pc.body = pc.body_raw.dup
       pc.save
     end
