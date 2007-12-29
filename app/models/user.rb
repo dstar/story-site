e105ca0a48d4b6dbc20d655b32264977401f13e3
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-29 09:31:38 +0000

    debug
    git-svn-id: svn://pele.cx/site/story/trunk@827 7189bf43-9712-0410-8374-e18a92618762

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,9 @@ class User < ActiveRecord::Base
     @story_permissions = [] unless @story_permissions  
     @story_permissions[story_id] = [] unless @story_permissions[story_id]
     @story_permissions[story_id] = self.story_permissions.find(:all, :conditions => "story_id = #{story_id}") if @story_permissions[story_id].empty?
+logger.debug "QQQ:@story_permissions[story_id] is #{@story_permissions[story_id]}"
     @story_permissions[story_id] = [@story_permissions[story_id]] if @story_permissions.class == StoryPermission
+logger.debug "QQQ:@story_permissions[story_id] is #{@story_permissions[story_id]}" if @story_permissions.class == StoryPermission
     obtained_permisson = @story_permissions[story_id].any? { |sp| sp.permission == permission}
     unless obtained_permisson
       self.groups.each do |group|
