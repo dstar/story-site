04ba1e2055b25d10c0462562afdaf1c5448aecac
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-29 07:51:41 +0000

    change 'and' and 'or' to '&&' and '||'
    git-svn-id: svn://pele.cx/site/story/trunk@809 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -1,6 +1,6 @@
 module StoriesHelper
   def Wordcount(story_id)
-    if @authinfo[:user] and ( StoryPermission.has_permission(@authinfo[:user], { 'id' => story_id, 'permission' => 'author'}) or StoryPermission.has_permission(@authinfo[:user], { 'id' => story_id, 'permission' => 'beta-reader'}) )
+    if @authinfo[:user] && ( StoryPermission.has_permission(@authinfo[:user], { 'id' => story_id, 'permission' => 'author'}) || StoryPermission.has_permission(@authinfo[:user], { 'id' => story_id, 'permission' => 'beta-reader'}) )
       Story.count_by_sql ["select sum(words) from chapters where story_id = ?", story_id]
     else
       Story.count_by_sql ["select sum(words) from chapters where story_id = ? AND status = 'released'", story_id]
@@ -9,7 +9,7 @@ module StoriesHelper
 
   def nav_links(story)
     nav_buffer = ""
-    if @authinfo[:user] and (story and @authinfo[:user].has_universe_permission(@universe.id,'owner') or @authinfo[:user].has_story_permission(story.id,'author') )
+    if @authinfo[:user] && (story && @authinfo[:user].has_universe_permission(@universe.id,'owner') || @authinfo[:user].has_story_permission(story.id,'author') )
       nav_buffer += link_to 'Edit Story Permissions', :controller => 'stories', :action => 'permissions', :id => story.id
       nav_buffer +=  " | "
       nav_buffer += link_to 'Edit Story', :action => 'edit', :id => story
@@ -22,7 +22,7 @@ module StoriesHelper
   end
 
   def can_delete?(story)
-    if @authinfo[:user] and (story and @authinfo[:user].has_universe_permission(@universe.id,'owner') or @authinfo[:user].has_story_permission(story.id,'author') )
+    if @authinfo[:user] && (story && @authinfo[:user].has_universe_permission(@universe.id,'owner') || @authinfo[:user].has_story_permission(story.id,'author') )
       return true
     else
       return false
@@ -30,21 +30,21 @@ module StoriesHelper
   end
 
   def can_comment(chapter)
-    if is_author(chapter) or is_beta_reader(chapter)
+    if is_author(chapter) || is_beta_reader(chapter)
       return true
     end
     return false
   end
 
   def is_author(chapter)
-    if @authinfo[:user] and @authinfo[:user].has_story_permission(chapter.story.id,'author')
+    if @authinfo[:user] && @authinfo[:user].has_story_permission(chapter.story.id,'author')
       return true
     end
     return false
   end
 
   def is_beta_reader(chapter)
-    if @authinfo[:user] and @authinfo[:user].has_story_permission(chapter.story.id,'beta-reader')
+    if @authinfo[:user] && @authinfo[:user].has_story_permission(chapter.story.id,'beta-reader')
       return true
     end
     return false
@@ -56,7 +56,7 @@ module StoriesHelper
     link_buffer += link_to "(Edit)", :controller => 'chapters', :action => 'edit', :id => chapter.id if is_author(chapter)
     link_buffer += " "
     
-    if chapter.status == 'released' or can_comment(chapter)
+    if chapter.status == 'released' || can_comment(chapter)
       link_buffer += "<em><strong>" if chapter.status == 'draft'
       link_buffer += link_to "Part #{chapter.number}", chapter_url(:chapter => chapter)
       link_buffer += " "
@@ -66,16 +66,16 @@ module StoriesHelper
 
     link_buffer += link_to("Comment ", :controller => 'chapters', :action => 'show_draft', :id => chapter.id) if can_comment(chapter)
 
-    link_buffer += "(#{chapter.date}, #{chapter.words} words" if chapter.status == 'released' or can_comment(chapter)
+    link_buffer += "(#{chapter.date}, #{chapter.words} words" if chapter.status == 'released' || can_comment(chapter)
 
     comment_count = Paragraph.count_by_sql(["select count(*) from paragraphs p, pcomments c where p.chapter_id=? and c.paragraph_id = p.id and c.flag != 2",chapter.id])
     link_buffer += ", #{comment_count} comments" if can_comment(chapter)
 
-    link_buffer += ", <strong>#{chapter.get_num_comments_unread_by(@authinfo[:username])} unread</strong>" if chapter.get_num_comments_unread_by(@authinfo[:username]) > 0 and can_comment(chapter)
+    link_buffer += ", <strong>#{chapter.get_num_comments_unread_by(@authinfo[:username])} unread</strong>" if chapter.get_num_comments_unread_by(@authinfo[:username]) > 0 && can_comment(chapter)
 
-    link_buffer += ", <span class=\"unacknowledged_count\">#{chapter.get_num_unacknowledged_comments} unacknowledged</span>" if is_author(chapter) and chapter.get_num_unacknowledged_comments > 0
+    link_buffer += ", <span class=\"unacknowledged_count\">#{chapter.get_num_unacknowledged_comments} unacknowledged</span>" if is_author(chapter) && chapter.get_num_unacknowledged_comments > 0
 
-    link_buffer += ") <br/>\n" if chapter.status == "released" or can_comment(chapter)
+    link_buffer += ") <br/>\n" if chapter.status == "released" || can_comment(chapter)
 
     return link_buffer
   end
@@ -83,7 +83,7 @@ module StoriesHelper
   def breadcrumbs
     home_link = link_to 'Home', index_url(:host => StoryHost('playground')) 
 
-    unless params[:action] == 'new' or params[:action] == 'create' or params[:action] == 'list' or params[:action] == 'index' 
+    unless params[:action] == 'new' || params[:action] == 'create' || params[:action] == 'list' || params[:action] == 'index' 
       universe_link = link_to @story.universe.name, "#{index_url(:host => StoryHost('playground'))}universes/show/#{@story.universe.id}" 
       if params[:action] == 'show' 
         title_link = "#{@story.title}"
@@ -93,7 +93,7 @@ module StoriesHelper
       end 
       return "#{home_link} &gt; #{universe_link} &gt; #{title_link}#{action_link}"
     else 
-      if params[:action] == 'list' or params[:action] == 'index' 
+      if params[:action] == 'list' || params[:action] == 'index' 
         return "#{home_link} &gt; Story List"
       else 
         universe_link = link_to @universe.name, "#{index_url(:host => StoryHost('playground'))}universes/show/#{@universe.id}"
