0614158742863c6cd4a6abacf8e5fd592e36a985
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2007-12-30 19:28:53 +0000

    use pcomment#{@paragraph.id} anchors in redirects, as some browsers can't find empty anchors
    git-svn-id: svn://pele.cx/site/story/trunk@852 7189bf43-9712-0410-8374-e18a92618762

--- app/controllers/paragraphs_controller.rb
+++ app/controllers/paragraphs_controller.rb
@@ -125,8 +125,8 @@ class ParagraphsController < ApplicationController
         @paragraph.chapter.update_attribute("words",word_count)
         dump_to_file(@paragraph.chapter)
         expire_fragment( :action => "show", :action_suffix => "paragraph_#{@paragraph.id}", :controller => "chapters" )
-        logger.debug("redirecting to #{url_for :controller => 'chapters', :action => 'show_draft', :id => @paragraph.chapter.id, :anchor => "p#{@paragraph.id}"}")
-        redirect_to :controller => 'chapters', :action => 'show_draft', :id => @paragraph.chapter.id, :anchor => "p#{@paragraph.id}"
+        logger.debug("redirecting to #{url_for :controller => 'chapters', :action => 'show_draft', :id => @paragraph.chapter.id, :anchor => "pcomment#{@paragraph.id}"}")
+        redirect_to :controller => 'chapters', :action => 'show_draft', :id => @paragraph.chapter.id, :anchor => "pcomment#{@paragraph.id}"
       else
         render :action => 'edit'
       end
@@ -149,7 +149,7 @@ class ParagraphsController < ApplicationController
 
     dump_to_file(next_paragraph.chapter)
 
-    redirect_to :controller => 'chapters', :action => 'show_draft', :id => next_paragraph.chapter.id, :anchor => "p#{next_paragraph.id}"
+    redirect_to :controller => 'chapters', :action => 'show_draft', :id => next_paragraph.chapter.id, :anchor => "pcomment#{next_paragraph.id}"
   end
 
   def setup_page_vars
@@ -187,7 +187,7 @@ class ParagraphsController < ApplicationController
         comment.save
       end
     end
-    redirect_to "#{index_url}chapters/show_draft/#{paragraph.chapter.id}#p#{paragraph.id}"
+    redirect_to "#{index_url}chapters/show_draft/#{paragraph.chapter.id}#pcomment#{paragraph.id}"
   end
   
 end
