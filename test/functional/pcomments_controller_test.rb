954e8f420ade1294ca2ac73a937c6866c6fdfd82
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2008-01-01 18:53:49 +0000

    controller is chapters, not show_draft....
    git-svn-id: svn://pele.cx/site/story/trunk@923 7189bf43-9712-0410-8374-e18a92618762

--- test/functional/pcomments_controller_test.rb
+++ test/functional/pcomments_controller_test.rb
@@ -99,7 +99,7 @@ class PcommentsControllerTest < Test::Unit::TestCase
 
     post :update, :id => 1
     assert_response :redirect
-    assert_redirected_to :controller => 'show_draft', :action => 'show_draft'
+    assert_redirected_to :controller => 'chapters', :action => 'show_draft'
   end
 
   def test_destroy_unauthed
