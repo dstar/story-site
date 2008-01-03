c3d1c445c8784b6a662705a8da52aecb310a2591
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2008-01-03 03:50:53 +0000

    fix chapter links
    git-svn-id: svn://pele.cx/site/story/trunk@1045 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/stories_helper.rb
+++ app/helpers/stories_helper.rb
@@ -55,10 +55,10 @@ module StoriesHelper
 
     link_buffer += link_to "(Edit)", :controller => 'chapters', :action => 'edit', :id => chapter.id if is_author(chapter)
     link_buffer += " "
-    
+
     if chapter.status == 'released' || can_comment(chapter)
       link_buffer += "<em><strong>" if chapter.status == 'draft'
-      link_buffer += link_to "Part #{chapter.number}", chapter_url(:chapter => chapter)
+      link_buffer += link_to "Part #{chapter.number}", chapter_url(:chapter => chapter.file.gsub(/.html/,''))
       link_buffer += " "
       link_buffer += "DRAFT</em></strong> " if chapter.status == 'draft'
       link_buffer += "<em>NEW!</em> " if (Date.today - chapter.date < 7)
@@ -81,26 +81,26 @@ module StoriesHelper
   end
 
   def breadcrumbs
-    home_link = link_to 'Home', index_url(:host => StoryHost('playground')) 
+    home_link = link_to 'Home', index_url(:host => StoryHost('playground'))
 
-    unless params[:action] == 'new' || params[:action] == 'create' || params[:action] == 'list' || params[:action] == 'index' 
-      universe_link = link_to @story.universe.name, "#{index_url(:host => StoryHost('playground'))}universes/show/#{@story.universe.id}" 
-      if params[:action] == 'show' 
+    unless params[:action] == 'new' || params[:action] == 'create' || params[:action] == 'list' || params[:action] == 'index'
+      universe_link = link_to @story.universe.name, "#{index_url(:host => StoryHost('playground'))}universes/show/#{@story.universe.id}"
+      if params[:action] == 'show'
         title_link = "#{@story.title}"
-      else 
-        title_link = link_to @story.title, "#{index_url(:host => StoryHost('playground'))}stories/show/#{@story.id}" 
-        action_link = " &gt; #{@description}" if @description 
-      end 
+      else
+        title_link = link_to @story.title, "#{index_url(:host => StoryHost('playground'))}stories/show/#{@story.id}"
+        action_link = " &gt; #{@description}" if @description
+      end
       return "#{home_link} &gt; #{universe_link} &gt; #{title_link}#{action_link}"
-    else 
-      if params[:action] == 'list' || params[:action] == 'index' 
+    else
+      if params[:action] == 'list' || params[:action] == 'index'
         return "#{home_link} &gt; Story List"
-      else 
+      else
         universe_link = link_to @universe.name, "#{index_url(:host => StoryHost('playground'))}universes/show/#{@universe.id}"
         return "#{home_link} &gt; #{universe_link}"
-      end 
+      end
 
     end
   end
-  
+
 end
