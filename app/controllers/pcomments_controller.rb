a8b93879e6b6a0db10d1685c3e545c33d4c6c1c4
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2008-01-21 00:17:29 +0000

    expire paragraph fragment on acknowledge/unack
    git-svn-id: svn://pele.cx/site/story/trunk@1075 7189bf43-9712-0410-8374-e18a92618762

--- app/controllers/pcomments_controller.rb
+++ app/controllers/pcomments_controller.rb
@@ -139,6 +139,7 @@ class PcommentsController < ApplicationController
       @pcomment.acknowledged = @authinfo[:user].username
       @pcomment.readers.push(@authinfo[:user])
       @pcomment.save
+      expire_fragment("show_pcomment_#{@pcomment.id}")
       if request.xml_http_request?
         @chapter = @paragraph.chapter
         render :partial => 'chapters/comment_block', :controller => "chapter", :locals => {:para => @pcomment.paragraph, :display => "block"}
@@ -155,6 +156,7 @@ class PcommentsController < ApplicationController
       @pcomment.acknowledged = ""
       @pcomment.readers.delete(@authinfo[:user])
       @pcomment.save
+      expire_fragment("show_pcomment_#{@pcomment.id}")
       if request.xml_http_request?
         @chapter = @paragraph.chapter
         render :partial => 'chapters/comment_block', :controller => "chapter", :locals => {:para => @pcomment.paragraph, :display => "block"}
