4116650347a006389512d58e9cd670327546c387
dstar <dstar@7189bf43-9712-0410-8374-e18a92618762>
2008-04-27 21:17:07 +0000

    Fix next/prev chapter determination -- use find :first with :conditions rather than count_by_sql, as min(number) returns 1 when there's only one chapter, rather than null.
    git-svn-id: svn://pele.cx/site/story/trunk@1099 7189bf43-9712-0410-8374-e18a92618762

--- app/helpers/chapters_helper.rb
+++ app/helpers/chapters_helper.rb
@@ -1,11 +1,9 @@
 module ChaptersHelper
   def nextChapter(chapter)
-    number = Chapter.count_by_sql(["select min(number) from chapters where story_id = ? and number > ?",chapter.story_id, chapter.number])
-    Chapter.find(:first, :conditions => ["story_id = ? and number = ?",chapter.story_id, number])
+    Chapter.find(:first, :conditions => ["story_id = ? and number > ?",chapter.story_id, number])
   end
   def prevChapter(chapter)
-    number = Chapter.count_by_sql(["select max(number) from chapters where story_id = ? and number < ?",chapter.story_id, chapter.number])
-    Chapter.find(:first, :conditions => ["story_id = ? and number = ?",chapter.story_id, number])
+    Chapter.find(:first, :conditions => ["story_id = ? and number < ?",chapter.story_id, number])
   end
 
   def chapter_navigation(chapter)
