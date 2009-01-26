6cf84337f8051eca269620e7ff2ae50cb25a14c9
Shalon Wood <dstar@pele.cx>
2009-01-26 10:52:03 -0600

    Add schema/development_structure.sql

---
+++ schema/development_structure.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `schema_migrations` (
+  `version` varchar(255) NOT NULL,
+  UNIQUE KEY `unique_schema_migrations` (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO schema_migrations (version) VALUES ('0');
