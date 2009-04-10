03af110fd6b6ee799c7b3d5a6aeddabe6c81351d
Shalon Wood <dstar@pele.cx>
2009-04-10 08:38:15 -0500

    Switch to author rather than universe links in breadcrumbs

--- app/helpers/pcomments_helper.rb
+++ app/helpers/pcomments_helper.rb
@@ -4,15 +4,17 @@ module PcommentsHelper
     story_index_url = "http://#{StoryHost(@pcomment.paragraph.chapter.story)}/"
     home_link = link_to 'Home', "http://#{StoryHost('playground')}/"
     if params[:id]
-      universe_link = link_to @pcomment.paragraph.chapter.story.universe.name, "#{story_index_url}universes/show/#{@pcomment.paragraph.chapter.story.universe.id} "
-      story_link = link_to @pcomment.paragraph.chapter.story.title, "#{story_index_url}stories/show/#{@pcomment.paragraph.chapter.story.id}"
+      story = @pcomment.paragraph.chapter.story
+      author_link = link_to story.authors.first.username, "/authors/show/#{story.authors.first.id}"
+      story_link = link_to story.title, "/stories/show/#{story.id}"
       chapter_link = link_to @pcomment.paragraph.chapter.number, "#{story_index_url}chapters/show/#{@pcomment.paragraph.chapter.id}"
     else
-      universe_link = link_to @paragraph.chapter.story.universe.name, "#{story_index_url}universes/show/#{@paragraph.chapter.story.universe.id}"
-      story_link = link_to @paragraph.chapter.story.title, "#{story_index_url}stories/show/#{@paragraph.chapter.story.id}"
-      chapter_link = link_to @paragraph.chapter.number, "#{story_index_url}chapters/show/#{@paragraph.chapter.id}"
+      story = @paragraph.chapter.story
+      author_link = link_to story.authors.first.username, "/authors/show/#{story.authors.first.id}"
+      story_link = link_to story.title, "/stories/show/#{story.id}"
+      chapter_link = link_to @paragraph.chapter.number, "/chapters/show/#{@paragraph.chapter.id}"
     end
-    return "#{home_link} &gt; #{universe_link} &gt; #{story_link} &gt; #{chapter_link}"
+    return "#{home_link} &gt; #{author_link} &gt; #{story_link} &gt; #{chapter_link}"
   end
 end
 end
