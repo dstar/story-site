97610b9767ccbf0e4cdb764f1cd213a769a85ce1
Shalon Wood <dstar@pele.cx>
2009-04-11 08:20:42 -0500

    add expiration changes; destroy chapters when story is destroyed; add scoped has_many for released_chapters, authors, and beta_readers

--- app/models/story.rb
+++ app/models/story.rb
@@ -3,22 +3,29 @@ class Story < ActiveRecord::Base
   validates_uniqueness_of :title, :short_title
   validates_format_of :on_release, :with => /[a-zA-Z0-9_]*/
 
+  after_save :expire_story_fragment
+  after_save :notify_universe_of_change
+
   belongs_to :universe
 
-  has_many :chapters
+  has_many :chapters, :dependent => :destroy
+  has_many :released_chapters, :conditions => "status = 'released'"
 
   has_many :story_permissions
   has_many :required_permissions, :as => :permissionable
+  has_many :authors, :through => :story_permissions, :source => :permission_holder, :source_type => 'User', :conditions => "story_permissions.permission = 'author'"
+  has_many :beta_readers, :through => :story_permissions, :source => :permission_holder, :source_type => 'User', :conditions => "story_permissions.permission = 'beta-reader'"
 
   serialize :on_release
   serialize :required_permissions
 
   #hack, because I can't figure out a way to make has_many :authors work
   has_many :credits
-  has_many :users, :through => :credits
-  def authors
-    self.users.collect { |u| c = Credit.find_by_user_id_and_story_id(u.user_id,self.id); u if c.credit_type == 'Author' }.compact
-  end
+
+#  def authors
+#    self.users.collect { |u| c = Credit.find_by_user_id_and_story_id(u.user_id,self.id); u if c.credit_type == 'Author' }.compact
+#    self.users(:conditions =>)
+#  end
 
   def self.OrderedListByUniverse(universe_id)
     find(:all,
@@ -37,7 +44,7 @@ class Story < ActiveRecord::Base
   def required_permission(action)
     return self.required_permissions.find(:all, :conditions => "status = '#{self.status}' and action = '#{action}'").collect { |perm| perm.permission}
   end
-  
+
   def self.default_permissions
     return { 'draft' => {
         "destroy" => [ "author", ],
@@ -70,8 +77,27 @@ class Story < ActiveRecord::Base
         "show"    => [ "EVERYONE"],
         "showByName"    => [ "EVERYONE"],
         "showBySubD"    => [ "EVERYONE"],
-      } 
+      }
     }
   end
-  
+
+  def cache_key
+    "story_#{self.id}"
+  end
+
+  def chapter_updated
+    expire_story_fragment
+    notify_universe_of_change
+  end
+
+  private
+  def expire_story_fragment
+    expire(self.cache_key)
+    expire("story_list#{self.id}#true")
+    expire("story_list#{self.id}#false")
+  end
+
+  def notify_universe_of_change
+    self.universe.story_updated if self.universe
+  end
 end
