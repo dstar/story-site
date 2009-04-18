deaf90d6f001a065f384878067b2c67420346745
Shalon Wood <dstar@pele.cx>
2009-04-17 20:38:08 -0500

    Spec for processing text file

--- spec/models/chapter_spec.rb
+++ spec/models/chapter_spec.rb
@@ -1,7 +1,16 @@
 require File.join( File.dirname(__FILE__), "..", "spec_helper" )
 
 describe Chapter do
-#  fixtures :chapters, :paragraphs, :pcomments, :users
+  test_files_dir = File.join( File.dirname(__FILE__), "..", "..", "features", "test_files" )
+  regexps = [Regexp.new("^This is test paragraph 1\\."),
+             Regexp.new("^This is _test paragraph 2_\\."),
+             Regexp.new("^This is \\*test paragraph 3\\*\\."),
+             Regexp.new("^This is _\\*test paragraph 4\\*_\\."),
+             Regexp.new("^This is \\*\\*test paragraph 5\\*\\*\\."),
+             Regexp.new("^This is __test paragraph 6__\\."),
+            ]
+
+  #  fixtures :chapters, :paragraphs, :pcomments, :users
 
   it "should find the right number of comments" do
     chapter = Chapter.find(17)
@@ -21,6 +30,18 @@ describe Chapter do
     pcomments_unread_count = pcomments.inject(0) { |i, p| unless p.readers.include?(user) then i += 1 else i end}
 
     chapter.num_unread_comments(user).should eql(pcomments_unread_count)
+  end
+
+  it "should process a text file correctly" do
+    chapter = Chapter.new
+    chapter.story = Story.find(:last)
+    chapter.save
+    file = File.open("#{test_files_dir}/test_chapter_1.txt",'r')
+    chapter.process_file(file)
+    chapter.paragraphs.length.should == 20
+    for paragraph in (0..5)
+      chapter.paragraphs[paragraph].body_raw.should match regexps[paragraph]
+    end
   end
 
 end
